2fa6f61715e356cbe650248e78bb0b1349374f78
benavio <dt76tank@gmail.com>
2025-06-25 21:58:45 +0300

    alco

--- hw1/main.go
+++ hw1/main.go
@@ -1,9 +1,52 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go, World!")
+
+	Link("httsp://weqwehttp://qweqwe.com qwdqw http://qefqef*")
+
+}
+
+func Link(str string) {
+	newList := []byte(str)
+	marker := []byte("http://")
+
+	findLink(newList, marker)
+
+	fmt.Println(len([]byte("http://")) == len([]byte("htpt://")))
+	fmt.Println(newList)
+	fmt.Println(string(newList))
+}
+
+func findLink(newList []byte, marker []byte) {
+	for i := 0; i < len(newList); i++ {
+		if i+len(marker) > len(newList) {
+			break
+		}
+
+		match := true
+		for j := 0; j < len(marker); j++ {
+			if newList[i+j] != marker[j] {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			hideLink(i+len(marker), newList)
+			i += len(marker)
+		}
+	}
+
+}
+
+func hideLink(point int, newList []byte) {
+	for k := point; k < len(newList); k++ {
+		if newList[k] != 32 {
+			newList[k] = 42
+		} else {
+			break
+		}
+	}
 }
